2f712b4858336a9fbcdfce48182ecf7ace752d3a
mikeli <a15210183705@163.com>
2024-05-29 16:56:11 +0800

    修改

--- model.py
+++ model.py
@@ -2,9 +2,9 @@ import torch
 from torch import nn
 from transformers import BertForSequenceClassification, BertConfig
 
-class MyModel(nn.Module):
+class BertModel(nn.Module):
     def __init__(self):
-        super(MyModel, self).__init__()
+        super(BertModel, self).__init__()
         self.bert = BertForSequenceClassification.from_pretrained("bert-base-chinese", num_labels = 2)  # /bert_pretrain/
         self.device = torch.device("cuda")
         for param in self.bert.parameters():
@@ -15,5 +15,18 @@ class MyModel(nn.Module):
                               token_type_ids=batch_seq_segments, labels = labels)
         loss = output['loss']
         logits = output['logits']
+        probabilities = nn.functional.softmax(logits, dim=-1)
+        return loss, logits, probabilities
+
+class BertModelTest(nn.Module):
+    def __init__(self):
+        super(BertModelTest, self).__init__()
+        config = BertConfig.from_pretrained('models/config.json')
+        self.bert = BertForSequenceClassification(config)  # /bert_pretrain/
+        self.device = torch.device("cuda")
+
+    def forward(self, batch_seqs, batch_seq_masks, batch_seq_segments, labels):
+        loss, logits = self.bert(input_ids = batch_seqs, attention_mask = batch_seq_masks, 
+                              token_type_ids=batch_seq_segments, labels = labels)
         probabilities = nn.functional.softmax(logits, dim=-1)
         return loss, logits, probabilities
--- predict.py
+++ predict.py
@@ -1,71 +1,36 @@
-import os
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from tqdm import tqdm
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.optim import lr_scheduler
 from sklearn.metrics import roc_auc_score
 
 from load_data import DataPrecessForSentence
 from params import *
-from model import MyModel
+from model import BertModel
 
 class Trainer():
-    def __init__(self):
-        bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese', do_lower_case=True)
-        train_file = "/kaggle/input/ceshi05289963/train.txt"
-        train_data = DataPrecessForSentence(bert_tokenizer, train_file)
-        self.train_loader = DataLoader(train_data, batch_size=10, shuffle=False)
+    def __init__(self, model_path):
+        # 加载预训练模型的权重并移至 CPU
+        self.model = BertModel()
+        state_dict = torch.load(model_path, map_location='cpu')
+        self.model.load_state_dict(state_dict)
+        self.model.eval()
 
-        train_file = "/kaggle/input/ceshi05289963/val.txt"
-        dev_data = DataPrecessForSentence(bert_tokenizer, train_file)
+        bert_tokenizer = BertTokenizer.from_pretrained('bert-base-chinese', do_lower_case=True)
+        dev_file = "data/val.txt"
+        dev_data = DataPrecessForSentence(bert_tokenizer, dev_file)
         self.dev_loader = DataLoader(dev_data, batch_size=1, shuffle=False)
 
-        self.model = MyModel(2).to(DEVICE)
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=LEARNING_RATE, momentum=0.9)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
-
-    def train(self):
-        tbar = tqdm(self.train_loader)
-        self.model.train()
-        train_loss = 0.0
-        for batch_idx, data in enumerate(tbar):
-            batch_seqs, batch_seq_masks, batch_seq_segments, batch_labels = data
-            seqs, masks, segments, labels = batch_seqs.to(DEVICE), batch_seq_masks.to(DEVICE), batch_seq_segments.to(DEVICE), batch_labels.to(DEVICE)
-            self.optimizer.zero_grad()
-            loss, logits, probabilities = self.model(seqs, masks, segments, labels)
-            loss.backward()
-            self.scheduler.step()
-            train_loss += loss.item()
-            tbar.set_description("Loss: %.3f"%(train_loss / (batch_idx + 1)))
-
-        # torch.save(model, MODEL_DIR + f'model_{epoch}.pth')
-
-    def val(self):
-        self.model.eval()
-        all_pre = []
-        all_tag = []
+    def test(self):
         tbar = tqdm(self.dev_loader)
         with torch.no_grad():
-            for batch_idx, data in tqdm(tbar):
+            for batch_idx, data in tqdm(enumerate(tbar)):
                 batch_seqs, batch_seq_masks, batch_seq_segments, batch_labels = data
-                seqs, masks, segments, labels = batch_seqs.to(DEVICE), batch_seq_masks.to(DEVICE), batch_seq_segments.to(DEVICE), batch_labels.to(DEVICE)
-                self.optimizer.zero_grad()
+                seqs, masks, segments, labels = batch_seqs, batch_seq_masks, batch_seq_segments, batch_labels
                 loss, logits, probabilities = self.model(seqs, masks, segments, labels)
-                all_pre.extend(probabilities[:,1].cpu().numpy())
-                all_tag.extend(batch_labels)
-                val_loss += loss.item()
-                tbar.set_description("Loss: %.3f"%(val_loss / (batch_idx + 1)))
-        score = roc_auc_score(all_pre, all_tag)
-        print(score)
-
-        
-
+                print(probabilities)
 
 if __name__ == "__main__":
-    trainer = Trainer()
+    model_path = "model/best.pth.tar"  # 替换为你的.pth.tar模型文件路径
+    trainer = Trainer(model_path)
 
-    for epoch in range(EPOCHS):
-        trainer.train()
-        trainer.val()
+    trainer.test()
